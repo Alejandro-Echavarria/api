055cc1fb434f368d3aee1ff45cb7aa07863bf9c1
Manuel Echavarria <mechavarria@aldereca.com.do>
2023-01-27 17:50:56 -0400

    Avances

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\Auth\LoginController;
 
+Route::post('login', [LoginController::class, 'store'])->name('api.v1.login');
 Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
+
 Route::apiResource('categories', CategoryController::class)->names('api.v1.categories');
 Route::apiResource('posts', PostController::class)->names('api.v1.posts');
